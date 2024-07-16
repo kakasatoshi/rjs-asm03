62035c48ea007224e2af10c704e56d9beee686f0
kakasatoshi <anhsequayve922111@gmail.com>
2024-07-16 22:03:36 +0700

    Update Footer.js
    sữa footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -6,25 +6,145 @@ const Footer = () => {
     <div className="footer-content">
       <div className="footer">
         <div className="column">
-          <div className="row top">Hàng 1 Cột 1</div>
-          <div className="row bottom">Hàng 2 Cột 1</div>
-          <div className="row bottom">Hàng 3 Cột 1</div>
-          <div className="row bottom">Hàng 4 Cột 1</div>
-          <div className="row bottom">Hàng 5 Cột 1</div>
+          <div className="row top">
+            <a
+              href="#"
+              className="text-white"
+              style={{ textDecoration: "none", margin: 0 }}
+            >
+              CUSTOMER SERVICE
+            </a>
+          </div>
+          <div className="row bottom text-white">
+            <a
+              href="#"
+              className="text-white"
+              style={{ textDecoration: "none", margin: 0 }}
+            >
+              Help & Contact Us
+            </a>
+          </div>
+          <div className="row bottom text-white">
+            <a
+              href="#"
+              className="text-white"
+              style={{ textDecoration: "none", margin: 0 }}
+            >
+              Returns & Refunds
+            </a>
+          </div>
+          <div className="row bottom text-white">
+            <a
+              href="#"
+              className="text-white"
+              style={{ textDecoration: "none", margin: 0 }}
+            >
+              Online Stores
+            </a>
+          </div>
+          <div className="row bottom text-white">
+            <a
+              href="#"
+              className="text-white"
+              style={{ textDecoration: "none", margin: 0 }}
+            >
+              Terms & Conditions
+            </a>
+          </div>
         </div>
         <div className="column">
-          <div className="row top">Hàng 1 Cột 2</div>
-          <div className="row bottom">Hàng 2 Cột 2</div>
-          <div className="row bottom">Hàng 3 Cột 2</div>
-          <div className="row bottom">Hàng 4 Cột 2</div>
-          <div className="row bottom">Hàng 5 Cột 2</div>
+          <div className="row top text-white">
+            <a
+              href="#"
+              className="text-white"
+              style={{ textDecoration: "none", margin: 0 }}
+            >
+              COMPANY
+            </a>
+          </div>
+          <div className="row bottom text-white">
+            <a
+              href="#"
+              className="text-white"
+              style={{ textDecoration: "none", margin: 0 }}
+            >
+              What We Do
+            </a>
+          </div>
+          <div className="row bottom text-white">
+            <a
+              href="#"
+              className="text-white"
+              style={{ textDecoration: "none", margin: 0 }}
+            >
+              Available Service
+            </a>
+          </div>
+          <div className="row bottom text-white">
+            <a
+              href="#"
+              className="text-white"
+              style={{ textDecoration: "none", margin: 0 }}
+            >
+              Latest Posts
+            </a>
+          </div>
+          <div className="row bottom text-white">
+            <a
+              href="#"
+              className="text-white"
+              style={{ textDecoration: "none", margin: 0 }}
+            >
+              FAQs
+            </a>
+          </div>
         </div>
         <div className="column">
-          <div className="row top">Hàng 1 Cột 3</div>
-          <div className="row bottom">Hàng 2 Cột 3</div>
-          <div className="row bottom">Hàng 3 Cột 3</div>
-          <div className="row bottom">Hàng 4 Cột 3</div>
-          <div className="row bottom">Hàng 5 Cột 3</div>
+          <div className="row top text-white">
+            <a
+              href="#"
+              className="text-white"
+              style={{ textDecoration: "none", margin: 0 }}
+            >
+              SOCIAL MEDIA
+            </a>
+          </div>
+          <div className="row bottom text-white">
+            <a
+              href="#"
+              className="text-white"
+              style={{ textDecoration: "none", margin: 0 }}
+            >
+              Twitter
+            </a>
+          </div>
+          <div className="row bottom text-white">
+            <a
+              href="#"
+              className="text-white"
+              style={{ textDecoration: "none", margin: 0 }}
+            >
+              Instagram
+            </a>
+          </div>
+          <div className="row bottom text-white">
+            <a
+              href="#"
+              className="text-white"
+              style={{ textDecoration: "none", margin: 0 }}
+            >
+              Facebook
+            </a>
+          </div>
+          <div className="row bottom text-white">
+            <a
+              href="#"
+              className="text-white"
+              style={{ textDecoration: "none", margin: 0 }}
+            >
+              Pinterest
+            </a>
+          </div>
         </div>
       </div>
     </div>
